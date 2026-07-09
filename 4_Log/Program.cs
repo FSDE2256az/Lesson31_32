e603ba9065ef849d44f1025796c507dcc568485f
karimzade_k <karimzade_k@STHQ012D-01>
2026-07-09 21:00:13 +0400

    Log started

---
+++ 4_Log/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _4_Log;
+
+
+// Log => Env hamisinda proseslerin gedisati haqqinda Proqramist ucun lazimi olan melumatlardir
+
+// Audit Finding
+
+// Loglama etmek ucun meshur olan kitabxanalar
+
+// Serilog => Yeni texnologiya + Elastic Kibana 
+// NLog ( WebForm, Aspx )
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        
+
+
+
+
+        
+
+
+    }
+}
