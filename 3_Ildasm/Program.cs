eeee3bc955586c7ccdfbc3ed25cd5aeb69ddbfd3
karimzade_k <karimzade_k@STHQ012D-01>
2026-07-09 20:54:02 +0400

    Ildasm completed

---
+++ 3_Ildasm/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _3_Ildasm;
+
+
+class Person()
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 3_Ildasm/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace _3_Ildasm
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
