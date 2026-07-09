07d98a970a0102bd976a4f44be4b945d6a0e1d7d
karimzade_k <karimzade_k@STHQ012D-01>
2026-07-09 20:07:19 +0400

    Add project files.

---
+++ 1_Regex/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _1_Regex;
+
+// string email = "";
+
+/*
+ 
+
+*/
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        // string pattern = "^Kamran";
+        // string pattern = "^[a-zA-Z]+$";
+        // string pattern = "[A-Z]";
+        // string pattern = "[a-z]";
+        // string pattern = "^[0-9]+$";
+        // string pattern = "^[a-zA-Z0-9]+$";
+        // string pattern = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
+        // 
+        // string fullname = "15649Kamran@dd.az";
+        // 
+        // var regex = new Regex(pattern);
+        // 
+        // Console.WriteLine(regex.IsMatch(fullname));
+
+
+        string pattern = "(Mr\\.? |Mrs\\.? |Miss |Ms\\.? )";
+        string[] names = { "Mr. Henry Hunt", "Ms. Sara Samuels",
+                          "Abraham Adams", "Ms. Nicole Norris" };
+
+        foreach (string name in names)
+            Console.WriteLine(Regex.Replace(name, pattern, String.Empty));
+
+
+    }
+}
