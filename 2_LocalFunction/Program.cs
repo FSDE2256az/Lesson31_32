8714cb81f9a130f528089e4d5b95774ad929a9a1
karimzade_k <karimzade_k@STHQ012D-01>
2026-07-09 20:18:53 +0400

    Local Function completed

--- 2_LocalFunction/Program.cs
+++ 2_LocalFunction/Program.cs
@@ -1,10 +1,65 @@
-﻿namespace _2_LocalFunction
+﻿namespace _2_LocalFunction;
+
+internal class Program
 {
-    internal class Program
+
+    static void BooGlobal()
     {
-        static void Main(string[] args)
+        Console.WriteLine("BooGlobal Started");
+    }
+
+
+
+    static void Foo()
+    {
+        Console.WriteLine("Foo started");
+
+        Boo();
+
+        void Boo()
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Boo Started");
         }
+    }
+
+
+    static void Something(int value)
+    {
+        Console.WriteLine($"Value: {value}");
+
+
+        void Helper()
+        {
+            Console.WriteLine($"Helper: {value}");
+        }
+
+
+        static void HelperStatic()
+        {
+            int x = 10;
+            Console.WriteLine($"HelperStatic started: {x}");
+            // Console.WriteLine($"HelperStatic started: {value}"); // Error => static olmadigi ucun
+        }
+
+        static void HelperStaticWithValue(int param)
+        {
+            Console.WriteLine($"HelperStaticWithValue started: {param}");
+
+        }
+
+        Helper();
+        HelperStatic();
+        HelperStaticWithValue(value);
+    }
+
+
+    static void Main(string[] args)
+    {
+
+        // Foo();
+        // BooGlobal();
+        // Boo();
+
+        Something(42);
     }
 }
